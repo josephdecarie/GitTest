4e7b891d49ea39a35644da75d1a516f71e10e5da
Joseph Decarie <josephdecarie@MacBook-Pro-de-Joseph.local>
2013-03-03 11:20:41 -0500

    Initial Commit

---
+++ GitTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by Joseph Decarie on 2013-03-03.
+//  Copyright (c) 2013 Joseph Decarie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GitTest
+//
+//  Created by Joseph Decarie on 2013-03-03.
+//  Copyright (c) 2013 Joseph Decarie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
